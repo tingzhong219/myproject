fb53c062ab4d311f063172c5520ef29b3a1ad4cf
tingzhong <wangkai_java1@163.com>
2019-09-03 10:39:11 +0800

    local update file

--- myproject/my_shop_order_provider1/src/main/java/com/zb/MyShopOrderProviderApplication.java
+++ myproject/my_shop_order_provider1/src/main/java/com/zb/MyShopOrderProviderApplication.java
@@ -3,7 +3,7 @@ package com.zb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+//本地新开发的功能
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MyShopOrderProviderApplication {
